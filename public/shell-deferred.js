dd39f860dd40c8374b6b1f74bee78845954cd8e1
chavzwill <chavevil@gmail.com>
2026-08-24 15:48:21 -0500

    Refresh Guided Mode workflow completion engine

--- public/shell-deferred.js
+++ public/shell-deferred.js
@@ -1,5 +1,5 @@
 (()=>{'use strict';
-const VERSION='20260824-1543';
+const VERSION='20260824-1547';
 const loaded=new Set();
 const nativeFetch=window.fetch.bind(window);
 let profileResponsePromise=null;
