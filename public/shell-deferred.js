58efacd823ac7b93ebd9f4c1b2ddafc5dbf0cc8e
chavzwill <chavevil@gmail.com>
2026-08-25 15:25:41 -0500

    Bust Guided Mode runtime cache for exact-control coach

--- public/shell-deferred.js
+++ public/shell-deferred.js
@@ -1,5 +1,5 @@
 (()=>{'use strict';
-const VERSION='20260825-1938';
+const VERSION='20260825-2025';
 const loaded=new Set();
 const nativeFetch=window.fetch.bind(window);
 let profileResponsePromise=null;
