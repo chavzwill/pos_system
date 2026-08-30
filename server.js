5beb2309570039eee6c760d15f3d3d0a9c574100
chavzwill <chavevil@gmail.com>
2026-08-30 18:57:52 -0500

    Mount controlled rental refund settlement

--- server.js
+++ server.js
@@ -175,6 +175,7 @@ app.use('/api/denominations', require('./routes/denominations'));
 app.use('/api/woocommerce', woocommerceRouter);
 app.use('/api/api-keys', require('./routes/api-keys'));
 app.use('/api/rentals', require('./routes/rental-checkout-cash-drawer-guard'));
+app.use('/api/rentals', require('./routes/rental-refund-settlement'));
 app.use('/api/rentals', require('./routes/rental-loss-prevention'));
 app.use('/api/rentals', require('./routes/rentals'));
 app.use('/api/layaway', require('./routes/layaway'));
