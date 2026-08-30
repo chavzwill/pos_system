1334998a8577d3488eb4647be2461ded4285d604
chavzwill <chavevil@gmail.com>
2026-08-30 17:47:05 -0500

    Chain multi-branch certification into release checks

--- scripts/check-client-syntax.js
+++ scripts/check-client-syntax.js
@@ -36,3 +36,4 @@ require('./check-logistics-completion-mode-contract');
 require('./check-repair-lifecycle-certification');
 require('./check-accounting-financial-integrity-certification');
 require('./check-rbac-security-certification');
+require('./check-multi-branch-integrity-certification');
